948b3c70402d64d74a14df11a21be5b773ec9b99
Muhammet Tarık Öğüt <mutarikogut@gmail.com>
2024-07-31 11:22:38 +0300

    Design, Score increase and Countdown timer added

---
+++ app/src/main/java/com/mutarikogut/firstgameapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.mutarikogut.firstgameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView timeText;
+    TextView scoreText;
+    int score;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timeText = findViewById(R.id.timeText);
+        scoreText = findViewById(R.id.scoreText);
+        score = 0;
+
+        new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeText.setText("Time: " + millisUntilFinished/1000);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+    }
+
+    public void increaseScore(View view){
+        score++;
+        scoreText.setText("Score: "+score);
+
+    }
+}
