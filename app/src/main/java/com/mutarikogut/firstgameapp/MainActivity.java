dfdc3278a1b299da7a448934ee0d034bd0ba96e1
Muhammet Tarık Öğüt <mutarikogut@gmail.com>
2024-07-31 11:29:32 +0300

    Imageviews added array and is made invisible

--- app/src/main/java/com/mutarikogut/firstgameapp/MainActivity.java
+++ app/src/main/java/com/mutarikogut/firstgameapp/MainActivity.java
@@ -6,12 +6,23 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView timeText;
     TextView scoreText;
     int score;
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView[] imageArray;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -21,7 +32,18 @@ public class MainActivity extends AppCompatActivity {
         timeText = findViewById(R.id.timeText);
         scoreText = findViewById(R.id.scoreText);
         score = 0;
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
 
+        imageArray = new ImageView[] {imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9};
+        hideImages();
         new CountDownTimer(10000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -39,5 +61,11 @@ public class MainActivity extends AppCompatActivity {
         score++;
         scoreText.setText("Score: "+score);
 
+    }
+
+    public void hideImages(){
+        for(ImageView image: imageArray){
+            image.setVisibility(View.INVISIBLE);
+        }
     }
 }
